b967f6a2838a95a988d51d9684366176d62e4b96
Davron <davron.jabborov@gmail.com>
2026-02-17 15:52:48 -0500

    Initial Commit

---
+++ WakepromptTests/WakepromptTests.swift
@@ -0,0 +1,17 @@
+//
+//  WakepromptTests.swift
+//  WakepromptTests
+//
+//  Created by Davron Djabborov on 2026-02-17.
+//
+
+import Testing
+@testable import Wakeprompt
+
+struct WakepromptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
